c1839393d327ebed951fca0df2e67183d29d6b0a
Enrique Ramirez <enramire@ssspr.com>
2010-05-05 16:17:57 -0400

    Added GUI behaviors for control interaction.

--- src/ConversionSelect.Designer.cs
+++ src/ConversionSelect.Designer.cs
@@ -28,32 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.optSqlToSql = new System.Windows.Forms.RadioButton();
+            this.optVStoSql = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbxTo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxFrom = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCopySettings = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // optSqlToSql
+            // 
+            this.optSqlToSql.AutoSize = true;
+            this.optSqlToSql.Location = new System.Drawing.Point(12, 35);
+            this.optSqlToSql.Name = "optSqlToSql";
+            this.optSqlToSql.Size = new System.Drawing.Size(358, 17);
+            this.optSqlToSql.TabIndex = 0;
+            this.optSqlToSql.TabStop = true;
+            this.optSqlToSql.Text = "Copy between two different versions of Sql Server Management Studio";
+            this.optSqlToSql.UseVisualStyleBackColor = true;
+            this.optSqlToSql.CheckedChanged += new System.EventHandler(this.On_optSqlToSql_CheckedChanged);
+            // 
+            // optVStoSql
+            // 
+            this.optVStoSql.AutoSize = true;
+            this.optVStoSql.Location = new System.Drawing.Point(12, 12);
+            this.optVStoSql.Name = "optVStoSql";
+            this.optVStoSql.Size = new System.Drawing.Size(298, 17);
+            this.optVStoSql.TabIndex = 1;
+            this.optVStoSql.TabStop = true;
+            this.optVStoSql.Text = "Copy from Visual Studio to Sql Server Management Studio";
+            this.optVStoSql.UseVisualStyleBackColor = true;
+            this.optVStoSql.CheckedChanged += new System.EventHandler(this.On_optVStoSql_CheckedChanged);
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(13, 86);
+            this.groupBox1.Controls.Add(this.cbxTo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbxFrom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(12, 58);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
+            this.groupBox1.Size = new System.Drawing.Size(200, 72);
+            this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Version";
+            // 
+            // cbxTo
+            // 
+            this.cbxTo.FormattingEnabled = true;
+            this.cbxTo.Location = new System.Drawing.Point(45, 42);
+            this.cbxTo.Name = "cbxTo";
+            this.cbxTo.Size = new System.Drawing.Size(144, 21);
+            this.cbxTo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "To:";
+            // 
+            // cbxFrom
+            // 
+            this.cbxFrom.FormattingEnabled = true;
+            this.cbxFrom.Location = new System.Drawing.Point(45, 13);
+            this.cbxFrom.Name = "cbxFrom";
+            this.cbxFrom.Size = new System.Drawing.Size(144, 21);
+            this.cbxFrom.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "From:";
+            // 
+            // btnCopySettings
+            // 
+            this.btnCopySettings.Location = new System.Drawing.Point(248, 78);
+            this.btnCopySettings.Name = "btnCopySettings";
+            this.btnCopySettings.Size = new System.Drawing.Size(91, 23);
+            this.btnCopySettings.TabIndex = 3;
+            this.btnCopySettings.Text = "Copy Settings";
+            this.btnCopySettings.UseVisualStyleBackColor = true;
+            this.btnCopySettings.Click += new System.EventHandler(this.On_btnCopySettings_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(248, 107);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(91, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
             // ConversionSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 316);
+            this.ClientSize = new System.Drawing.Size(374, 139);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCopySettings);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.optVStoSql);
+            this.Controls.Add(this.optSqlToSql);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "ConversionSelect";
             this.Text = "ConversionSelect";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.RadioButton optSqlToSql;
+        private System.Windows.Forms.RadioButton optVStoSql;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCopySettings;
+        private System.Windows.Forms.Button btnClose;
+
     }
 }
--- src/ConversionSelect.cs
+++ src/ConversionSelect.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace VS_SQL_TextSchemeMigrator
 {
@@ -14,5 +15,97 @@ namespace VS_SQL_TextSchemeMigrator
         {
             InitializeComponent();
         }
+
+        private void On_optVStoSql_CheckedChanged(object sender, EventArgs e)
+        {
+            if (optVStoSql.Checked)
+                LoadVStoSqlComboBoxes();
+        }
+
+        private void On_optSqlToSql_CheckedChanged(object sender, EventArgs e)
+        {
+            if (optSqlToSql.Checked)
+                LoadSqlToSqlComboBoxes();
+        }
+
+        private void ResetComboBoxes()
+        {
+            cbxFrom.Items.Clear();
+            cbxTo.Items.Clear();
+        }
+
+        private void LoadVStoSqlComboBoxes()
+        {
+            string[] vsVersions = Enum.GetNames(typeof(VisualStudioVersion));
+            string[] sqlVersions = Enum.GetNames(typeof(SqlStudioVersion));
+
+            ResetComboBoxes();
+            cbxFrom.Items.AddRange(vsVersions);
+            cbxTo.Items.AddRange(sqlVersions);
+        }
+
+        private void LoadSqlToSqlComboBoxes()
+        { 
+            string[] sqlVersions = Enum.GetNames(typeof(SqlStudioVersion));
+
+            ResetComboBoxes();
+            cbxFrom.Items.AddRange(sqlVersions);
+            cbxTo.Items.AddRange(sqlVersions);
+        }
+
+        private void On_btnCopySettings_Click(object sender, EventArgs e)
+        {
+            if (!IsDataValid())
+                return;
+
+            if (optSqlToSql.Checked)
+            {
+                SqlStudioVersion source = (SqlStudioVersion)Enum.Parse(typeof(SqlStudioVersion), cbxFrom.SelectedItem.ToString());
+                SqlStudioVersion destination = (SqlStudioVersion)Enum.Parse(typeof(SqlStudioVersion), cbxTo.SelectedItem.ToString());
+                StartImportProcess(source, destination);
+            }
+
+            if (optVStoSql.Checked)
+            {
+                VisualStudioVersion source = (VisualStudioVersion)Enum.Parse(typeof(VisualStudioVersion), cbxFrom.SelectedItem.ToString());
+                SqlStudioVersion destination = (SqlStudioVersion)Enum.Parse(typeof(SqlStudioVersion), cbxTo.SelectedItem.ToString());
+                StartImportProcess(source, destination);
+            }
+        }
+
+        private bool IsDataValid()
+        {
+            bool returnValue = true;
+
+            if (!optSqlToSql.Checked && !optVStoSql.Checked)
+            {
+                MessageBox.Show("Please select the type of settings you want to copy.");
+                returnValue = false;
+            }
+
+            if (cbxFrom.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select the source of the settings you want to copy.");
+                cbxFrom.Focus();
+                returnValue = false;
+            }
+
+            if (cbxTo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select the destination of the settings you want to copy.");
+                cbxTo.Focus();
+                returnValue = false;
+            }
+
+            return returnValue;
+        }
+
+        private void StartImportProcess(VisualStudioVersion vsVersion, SqlStudioVersion sqlVersion)
+        { 
+            
+        }
+
+        private void StartImportProcess(SqlStudioVersion sqlSource, SqlStudioVersion sqlDestination)
+        { }
     }
 }
